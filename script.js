c9747ba71abe9ca4ca05fdff35ddec8ce5f226d1
R.Inthusha <inthusha.20241647@iit.ac.lk>
2026-09-17 22:25:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-var inputbox = document.querySelector(".inputbox");
-var btn = document.querySelector("#btn");
-var result = document.querySelector("#result");
-var randomNumber = Math.floor(Math.random() * 10) + 1;
-var guessCount = 0;
-var guessedNumbers=[]
-
-btn.addEventListener("click", function () {
-  var guess = Number(inputbox.value);
-  guessCount++;
-  guessedNumbers.push(guess);
-
-  if (randomNumber < guess) {
-    result.textContent = `Your guess is too high. 
-        No of guesses ${guessCount}.
-        guessed numbers ${guessedNumbers}`;
-  } else if (randomNumber == guess) {
-    result.textContent = `Yippie You Win!!  
-        No of guesses ${guessCount}.
-        guessed numbers ${guessedNumbers}`;
-  } else {
-    result.textContent = `Your guess is too low.
-         No of guesses ${guessCount}.
-         guessed numbers ${guessedNumbers}`;
-  }
-});
-
 function changeColor(){
     document.body.style.backgroundColor=
     `rgb(${Math.round(Math.random()*255)},
